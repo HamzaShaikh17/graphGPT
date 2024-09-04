9f13dcf419d3d6149b1c8e7371b0bfc7b8c84e8e
HamzaShaikh17 <hamzashaikhm123@gmail.com>
2024-09-04 16:18:52 +0530

    Add CORS headers

--- main.py
+++ main.py
@@ -11,8 +11,20 @@ from dotenv import load_dotenv
 from openai import OpenAI
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
-import uvicorn
 import json
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ['*']
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 
